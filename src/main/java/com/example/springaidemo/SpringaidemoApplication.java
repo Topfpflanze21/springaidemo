dda8fed1054d04da44141b90779576875c5dac28
Leon Luckerbauer <leon.luckerbauer@stud.it.htl-hl.ac.at>
2025-05-20 09:44:15 +0200

    Made the connection to spring ai with api key

--- src/main/java/com/example/springaidemo/SpringaidemoApplication.java
+++ src/main/java/com/example/springaidemo/SpringaidemoApplication.java
@@ -1,13 +1,26 @@
 package com.example.springaidemo;
 
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringaidemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringaidemoApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner commandLineRunner(ChatClient .Builder builder) {
+		return args -> {
+		    var client = builder.build();
+			var response = client.prompt("Tell me a joke").call().content();
+
+			System.out.println(response);
+		};
 	}
 
 }
